501961416d05dbe8e86f2764e58c92e1b6c36036
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-06-13 16:12:07 +0200

    Add: getProjects

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
+import axios from 'axios';
+
 <script>
 export default{
-
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted(){
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then(resp => {
+        this.projects = resp.data.results;
+        console.log(resp);
+      })
+    }
+  }
 }
 </script>
 
 <template>
-<h1>HelloWorld</h1>
+<h1>Tutti i progetti</h1>
 </template>
 
 <style lang="scss">
