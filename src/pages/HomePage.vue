d6e37be730fe881b8d66f74f7369bc51d8310e02
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-06-16 14:01:15 +0200

    Add: router and pages

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2>Home page</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/router.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "./pages/HomePage.vue";
+import ProjectsPage from "./pages/ProjectsPage.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+    {
+      path: "/projects",
+      name: "projects",
+      component: ProjectsPage,
+    }
+  ],
+});
+
+export { router };
