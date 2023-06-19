994d28f18b2dfd6dad6e3ac17e41ef42b4f74e6f
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-06-19 17:55:26 +0200

    Add: SingleProjectPage

--- src/pages/SingleProjectPage.vue
+++ src/pages/SingleProjectPage.vue
@@ -1,15 +1,36 @@
 <script>
+import axios from 'axios';
 export default {
-  name: "SingleProjectPage",
+  data() {
+    return {
+      name: "SingleProjectPage",
+      baseURL: "http://127.0.0.1:8000",
+      project: {}
+    }
+  },
   mounted() {
     const slug = this.$route.params.slug;
+    axios.get(`${this.baseURL}/api/projects/${slug}`).then(
+      resp => {
+        console.log(resp);
+        this.project = resp.data.results;
+      })
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    }
   }
 }
 </script>
 
 <template>
-  <div class="container">
-    <h2>Singolo Progetto</h2>
+  <div class="container py-4">
+    <h2>{{ project.title }}</h2>
+    <p>Slug: {{ project.slug }}</p>
+    <p>Contenuto: {{ project.content }}</p>
+
+    <a @click.prevent="goBack()" class="btn btn-primary" href="">Home</a>
   </div>
 </template>
 
